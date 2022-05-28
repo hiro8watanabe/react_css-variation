46ce58fe04312fed86f07919ccd07a900ab6fd9a
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-05-28 09:47:26 +0900

    Initial commit

---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,14 @@
+export const InlineStyle = () => {
+  const container = {
+    border: ""
+  };
+  const title = {};
+  const button = {};
+
+  return (
+    <div style={container}>
+      <p style={title}>Inline Style</p>
+      <button style={button}>FIGHT!</button>
+    </div>
+  );
+};
