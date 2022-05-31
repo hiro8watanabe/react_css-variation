dc4636249888b90278b476fe5bd61a4bda16516e
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-05-31 18:05:54 +0900

    inline-style reactにおけるinline-styleのあて方

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # react_css-variation
-
 Created with CodeSandbox
-
-React での色々な CSS のあてかた学習記録。
--- src/components/InlineStyle.jsx
+++ src/components/InlineStyle.jsx
@@ -1,13 +1,29 @@
 export const InlineStyle = () => {
   const container = {
-    border: "2px solid #333"
+    border: "2px solid #333",
+    borderRadius: "8px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
   };
-  const title = {};
-  const button = {};
+  const title = {
+    margin: "0",
+    color: "#333",
+    fontWeight: "bold"
+  };
+  const button = {
+    border: "0",
+    borderRadius: "8px",
+    backgroundColor: "blue",
+    padding: "8px",
+    color: "#fff"
+  };
 
   return (
     <div style={container}>
-      <p style={title}>Inline Style</p>
+      <p style={title}>- Inline Style -</p>
       <button style={button}>FIGHT!</button>
     </div>
   );
